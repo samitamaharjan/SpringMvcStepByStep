ca679e7ec555acfaed7817e8b013781071757041
Samita Maharjan <samitamaharjan@gmail.com>
2017-10-25 20:45:44 -0700

    Step11: basic spring mvc application using response body

---
+++ src/main/java/com/in28minutes/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.in28minutes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login")
+	@ResponseBody
+	public String login() {
+		return "Hello World!";
+	}
+}
