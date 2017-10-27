57cb6a616233719344d4a2be6e2240779af02d69
Samita Maharjan <samitamaharjan@gmail.com>
2017-10-26 21:40:42 -0700

    Step 15: Log4j logging

--- src/main/java/com/in28minutes/controller/LoginController.java
+++ src/main/java/com/in28minutes/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.in28minutes.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +13,18 @@ import service.AuthenticationService;
 @Controller
 public class LoginController {
 	
+	private final Logger LOGGER = Logger.getLogger(LoginController.class);
+	
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login() {
+		
+		// Test for log4j logging
+		LOGGER.error("ERROR message");
+		LOGGER.warn("WARN message");
+		LOGGER.info("INFO message");
+		LOGGER.debug("DEBUG message");
+		LOGGER.trace("TRACE message");
+		
 		return "login";
 	}
 
